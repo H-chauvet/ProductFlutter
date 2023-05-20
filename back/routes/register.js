abe90cbadbd83f0185c82da50511bcfe64b73b23
Henri Chauvet <henri.chauvet@epitech.eu>
2023-05-20 22:09:07 +0200

    [ADD] Password verification for register

--- back/routes/register.js
+++ back/routes/register.js
@@ -4,9 +4,13 @@ const userCtrl = require('../controllers/register')
 
 router.post('/register', async function (req, res, next) {
     try {
-        const { username, password } = req.body
-        if (!username || !password) {
-            return res.status(400).json({message: 'Username and Password are required.'});
+        const { username, password, confirmPassword } = req.body;
+        if (!username || !password || !confirmPassword) {
+            return res.status(400).json({ message: 'Username, Password, and Confirm Password are required.' });
+        }
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({ message: 'Passwords are not the same.' });
         }
 
         const existingUser = await userCtrl.findByUsername(username)
